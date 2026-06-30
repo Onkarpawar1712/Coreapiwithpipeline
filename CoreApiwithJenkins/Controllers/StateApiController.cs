6c3c34d2ea867afcf7995c6d39ee7860ebb00c14
Onkarpawar1712 <128963264+Onkarpawar1712@users.noreply.github.com>
2026-06-30 12:39:02 -0700

    Added

---
+++ CoreApiwithJenkins/Controllers/StateApiController.cs
@@ -0,0 +1,31 @@
+﻿using CoreApiwithJenkins.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiwithJenkins.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class StateApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/state")]
+
+        public List<State> GetState()
+
+        {
+            List<State> lst = new List<State>();
+            lst.Add(new State() {StateId = 101, Statename="Maharashtra"});
+            lst.Add(new State() { StateId = 102, Statename = "Gujarat" });
+            lst.Add(new State() { StateId = 103, Statename = "Uttar Pradesh" });
+            lst.Add(new State() { StateId = 104, Statename = "Odisa" });
+            lst.Add(new State() { StateId = 105, Statename = "Karnataka" });
+            lst.Add(new State() { StateId = 106, Statename = "Uttarpradesh" });
+            lst.Add(new State() { StateId = 107, Statename = "Nepal" });
+            lst.Add(new State() { StateId = 108, Statename = "Andhrapradesh" });
+
+            return lst;
+        }
+
+    }
+}
---
+++ CoreApiwithJenkins/Models/State.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreApiwithJenkins.Models
+{
+    public class State
+    {
+        public int StateId { get; set; }
+        public string Statename { get; set; }
+    }
+}
